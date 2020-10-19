d9887c12a54dd38c44c032c858ac6c8dbcf4e8d3
yash <ygaur697@gmail.com>
2020-10-19 15:49:11 +0530

    config server with in folder properties

---
+++ src/test/java/com/configserver/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
